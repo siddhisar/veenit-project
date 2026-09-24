39d4c9f179c7320f98533ef9ccd5795d70c3975e
devnco-siddhi <siddhi.sarote@devnco.co>
2026-09-24 18:06:03 +0530

    Remove Regulatory Alignment section; favicon and title updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import Header from '../components/Header.jsx'
 import Hero from '../components/Hero.jsx'
 import FeatureCards from '../components/FeatureCards.jsx'
-import ComplianceFramework from '../components/ComplianceFramework.jsx'
 import ISMSSection from '../components/ISMSSection.jsx'
 import ChainOfCustody from '../components/ChainOfCustody.jsx'
 import ContactSection from '../components/ContactSection.jsx'
@@ -19,7 +18,6 @@ export default function Home() {
       <main>
         <Hero />
         <FeatureCards />
-        <ComplianceFramework />
         <ISMSSection />
         <ChainOfCustody />
         <ClientLogos />
